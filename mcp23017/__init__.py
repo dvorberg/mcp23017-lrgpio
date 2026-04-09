c043a73ca511ba7bbf8bca69907495f063fafabf
Diedrich Vorberg <diedrich@tux4web.de>
2026-04-09 11:10:23 +0200

    Initial commit.

---
+++ mcp23017/__init__.py
@@ -0,0 +1,6 @@
+# Import the user-facing interface for convenience. 
+from .mcp23017 import Expander, Bank, Pin
+
+# These may be imported from here to convenience. 
+from i2cutils.bitpattern import ByteSpec, Byte
+
---
+++ mcp23017/abc.py
@@ -0,0 +1,11 @@
+# These stub classes provide forward declarations. 
+
+class Expander(object):
+    pass
+
+class Bank(object):
+    pass
+
+class Pin(object):
+    pass
+
