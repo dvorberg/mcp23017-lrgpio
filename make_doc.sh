b958c0eb4a88bd06a0202e70dcc289141bd06495
Diedrich Vorberg <diedrich@tux4web.de>
2026-04-09 11:17:48 +0200

    Take into account the new module layout.

--- make_doc.sh
+++ make_doc.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-if [ "!" -f mcp23017.py ]
+if [ "!" -d mcp23017 ]
 then
     echo "Must be run in project home."
     exit 255
 fi
 
-rm -f doc/*
+rm -R -f doc/*
 
 . .virtualenv.$(hostname -s)/bin/activate 
 
 PYTHONPATH=. pdoc -n -o doc/ -d google \
              --no-include-undocumented \
              --no-search \
-             mcp23017
+             mcp23017.mcp23017
